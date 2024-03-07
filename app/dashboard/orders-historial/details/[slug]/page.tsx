925657371f5385b8f59b2ef3d4ef4a1c789d3733
Camilo EurekaDreams <f20manuel@gmail.com>
2024-03-07 12:13:48 -0500

    slug

--- app/dashboard/orders-historial/details/[slug]/page.tsx
+++ app/dashboard/orders-historial/details/[slug]/page.tsx
@@ -2,7 +2,7 @@ import DashboardFooter from "@/app/component/DashboardFooter";
 import DashboardHeader from "@/app/component/DashboardHeader";
 import OrderDetailsContent from "@/app/component/OrderDetailsContent";
 
-const OrderDetails = ({ slug }: { slug: string }) => {
+const OrderDetails = ({ params }: { params: { slug: string } }) => {
   return (
     <main className="relative min-h-[93vh] w-full bg-gray-image bg-fixed bg-cover">
       <div className="bg-black bg-opacity-60 flex flex-col min-h-[93vh] w-full p-16 space-y-16">
