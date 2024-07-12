03a6642ae9cf256ff8e2a56379194f4f6644a4c4
diefer09 <fernando@eurekadreams.com>
2024-07-12 03:15:15 -0500

    feat: hiding credentials

--- rxcountry-backend/functions/src/services/firebaseService.js
+++ rxcountry-backend/functions/src/services/firebaseService.js
@@ -1,7 +1,23 @@
 /* eslint-disable linebreak-style */
 const admin = require("firebase-admin");
+require("dotenv").config();
 // const firebaseapp = require("firebase-admin/app");
-const serviceAccountKey = require("../../serviceAccountKey.json");
+// const serviceAccountKey = require("../../serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+
+const serviceAccountKey = {
+  type: serviceAccount.type,
+  project_id: serviceAccount.project_id,
+  private_key_id: serviceAccount.private_key_id,
+  private_key: serviceAccount.private_key,
+  client_email: serviceAccount.client_email,
+  client_id: serviceAccount.client_id,
+  auth_uri: serviceAccount.auth_uri,
+  token_uri: serviceAccount.token_uri,
+  auth_provider_x509_cert_url: serviceAccount.auth_provider_x509_cert_url,
+  client_x509_cert_url: serviceAccount.client_x509_cert_url,
+  universe_domain: serviceAccount.universe_domain,
+};
 
 // const firebaseService = admin.initializeApp({
 //   credential: firebaseapp.cert(serviceAccountKey),
@@ -10,7 +26,7 @@ const serviceAccountKey = require("../../serviceAccountKey.json");
 
 const firebaseService = admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
-  databaseURL: "https://rxcountry-backoffice.firebaseio.com",
+  databaseURL: process.env.DATABASE_URL,
 });
 
 const dbService = firebaseService.database();
--- shared/firebase/firebase.tsx
+++ shared/firebase/firebase.tsx
@@ -2,22 +2,36 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import { config as configDotenv } from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyC2yJFccHS8v9EFAuhJNS3XT5kFY2znL38",
-    authDomain: "rxcountry-backoffice.firebaseapp.com",
-    projectId: "rxcountry-backoffice",
-    storageBucket: "rxcountry-backoffice.appspot.com",
-    messagingSenderId: "150291031577",
-    appId: "1:150291031577:web:0b17b2757cecf3f6a6ac88",
-    measurementId: "G-NVG2EXRMDV",
-    backendBaseUrl:
-        "https://us-central1-rxcountry-backoffice.cloudfunctions.net/api",
-};
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC2yJFccHS8v9EFAuhJNS3XT5kFY2znL38",
+//     authDomain: "rxcountry-backoffice.firebaseapp.com",
+//     projectId: "rxcountry-backoffice",
+//     storageBucket: "rxcountry-backoffice.appspot.com",
+//     messagingSenderId: "150291031577",
+//     appId: "1:150291031577:web:0b17b2757cecf3f6a6ac88",
+//     measurementId: "G-NVG2EXRMDV",
+//     backendBaseUrl:
+//         "https://us-central1-rxcountry-backoffice.cloudfunctions.net/api",
+// };
+
+const firebaseConfigDev = JSON.parse(
+    process.env.NEXT_PUBLIC_FIREBASE_CONFIG as string,
+);
+const firebaseConfigPro = JSON.parse(
+    process.env.NEXT_PUBLIC_FIREBASE_CONFIG as string,
+);
+
+const firebaseConfig =
+    process.env.NODE_ENV === "production"
+        ? firebaseConfigPro
+        : firebaseConfigDev;
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
