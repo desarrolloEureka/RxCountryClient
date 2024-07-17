197d8bbbb64d8750d4db8e2f00ff27cd9a254275
diefer09 <fernando@eurekadreams.com>
2024-07-16 23:44:29 -0500

    feat: updated .env.production

--- lib/brevo/brevo.ts
+++ lib/brevo/brevo.ts
@@ -4,8 +4,7 @@ const apiInstance = new brevo.TransactionalEmailsApi();
 
 apiInstance.setApiKey(
     brevo.TransactionalEmailsApiApiKeys.apiKey,
-    "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",
-    // process.env.BREVO_API_KEY as string,
+    process.env.BREVO_API_KEY as string,
 );
 
 const smtpEmail = new brevo.SendSmtpEmail();
