77ee6581541586e1b7c27431f77d08c18d242376
diefer09 <fernando@eurekadreams.com>
2024-08-12 15:48:07 -0500

    feat: images auto load script part 1

--- shared/firebase/firebase.ts
+++ shared/firebase/firebase.ts
@@ -2,6 +2,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 import { config as configDotenv } from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,6 +10,17 @@ import { config as configDotenv } from "dotenv";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC2yJFccHS8v9EFAuhJNS3XT5kFY2znL38",
+//     authDomain: "rxcountry-backoffice.firebaseapp.com",
+//     projectId: "rxcountry-backoffice",
+//     storageBucket: "rxcountry-backoffice.appspot.com",
+//     messagingSenderId: "150291031577",
+//     appId: "1:150291031577:web:0b17b2757cecf3f6a6ac88",
+//     measurementId: "G-NVG2EXRMDV",
+//     backendBaseUrl:
+//         "https://us-central1-rxcountry-backoffice.cloudfunctions.net/api",
+// };
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -26,5 +38,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
+const storage = firebaseApp.storage();
 
-export { auth, db, firebaseConfig };
+export { auth, db, firebaseConfig, storage };
