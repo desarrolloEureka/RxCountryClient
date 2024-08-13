ebcd8463135d190c4af464c90aef7f32a4e1a2cd
diefer09 <fernando@eurekadreams.com>
2024-08-13 17:29:59 -0500

    feat: Automatic image import added

--- shared/firebase/firebase.ts
+++ shared/firebase/firebase.ts
@@ -3,24 +3,16 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
-import { config as configDotenv } from "dotenv";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC2yJFccHS8v9EFAuhJNS3XT5kFY2znL38",
-//     authDomain: "rxcountry-backoffice.firebaseapp.com",
-//     projectId: "rxcountry-backoffice",
-//     storageBucket: "rxcountry-backoffice.appspot.com",
-//     messagingSenderId: "150291031577",
-//     appId: "1:150291031577:web:0b17b2757cecf3f6a6ac88",
-//     measurementId: "G-NVG2EXRMDV",
-//     backendBaseUrl:
-//         "https://us-central1-rxcountry-backoffice.cloudfunctions.net/api",
-// };
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
